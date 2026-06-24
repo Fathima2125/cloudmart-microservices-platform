2290661577c6910ac851edba8475e85f16c30eb6
Fathima Yosra <fathimayosra25@gmail.com>
2026-06-24 19:48:37 +0400

    Add Terraform VPC foundation

---
+++ infra/terraform/environments/dev/variables.tf
@@ -0,0 +1,9 @@
+variable "aws_region" {
+  description = "AWS region for CloudMart infrastructure"
+  type        = string
+}
+
+variable "project_name" {
+  description = "Project name"
+  type        = string
+}
