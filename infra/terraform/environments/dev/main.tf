4fdb1e72f4cbc186f22393eaa3ead3fcb67d53e0
Fathima Yosra <fathimayosra25@gmail.com>
2026-06-25 09:23:06 +0400

    Add Terraform EKS cluster module

--- infra/terraform/environments/dev/main.tf
+++ infra/terraform/environments/dev/main.tf
@@ -27,3 +27,12 @@ module "security_groups" {
   project_name = var.project_name
   vpc_id       = module.vpc.vpc_id
 }
+
+module "eks" {
+  source = "../../modules/eks"
+
+  project_name       = var.project_name
+  subnet_ids         = module.vpc.public_subnet_ids
+  eks_sg_id          = module.security_groups.eks_sg_id
+  node_instance_type = "t3.medium"
+}
--- infra/terraform/environments/dev/outputs.tf
+++ infra/terraform/environments/dev/outputs.tf
@@ -21,3 +21,15 @@ output "rds_sg_id" {
 output "redis_sg_id" {
   value = module.security_groups.redis_sg_id
 }
+
+output "eks_cluster_name" {
+  value = module.eks.cluster_name
+}
+
+output "eks_cluster_endpoint" {
+  value = module.eks.cluster_endpoint
+}
+
+output "eks_node_group_name" {
+  value = module.eks.node_group_name
+}
