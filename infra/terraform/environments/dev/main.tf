b3691a573a33d73f51695930702934914a9e6970
Fathima Yosra <fathimayosra25@gmail.com>
2026-06-25 07:18:24 +0400

    Add Terraform security groups

--- infra/terraform/environments/dev/main.tf
+++ infra/terraform/environments/dev/main.tf
@@ -20,3 +20,10 @@ module "vpc" {
     "us-east-1b"
   ]
 }
+
+module "security_groups" {
+  source = "../../modules/security-groups"
+
+  project_name = var.project_name
+  vpc_id       = module.vpc.vpc_id
+}
--- infra/terraform/environments/dev/outputs.tf
+++ infra/terraform/environments/dev/outputs.tf
@@ -9,3 +9,15 @@ output "public_subnet_ids" {
 output "private_subnet_ids" {
   value = module.vpc.private_subnet_ids
 }
+
+output "eks_sg_id" {
+  value = module.security_groups.eks_sg_id
+}
+
+output "rds_sg_id" {
+  value = module.security_groups.rds_sg_id
+}
+
+output "redis_sg_id" {
+  value = module.security_groups.redis_sg_id
+}
