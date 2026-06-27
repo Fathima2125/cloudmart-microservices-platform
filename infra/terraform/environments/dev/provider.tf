3c871fd470c9b7117af21383153c5bfa973e250f
Fathima Yosra <fathimayosra25@gmail.com>
2026-06-27 22:27:09 +0400

    Configure Terraform remote backend with S3 and DynamoDB

--- infra/terraform/environments/dev/provider.tf
+++ infra/terraform/environments/dev/provider.tf
@@ -1,6 +1,14 @@
 terraform {
   required_version = ">= 1.6.0"
 
+  backend "s3" {
+    bucket         = "cloudmart-terraform-state-506098131053"
+    key            = "dev/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "cloudmart-terraform-locks"
+    encrypt        = true
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
